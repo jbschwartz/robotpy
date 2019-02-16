5643a4d2e70724d6763e3e8d2c5a8c850bb1b0a2
James Schwartz <jbschwartz@users.noreply.github.com>
2019-02-16 14:10:14 -0500

    Test default Frame construction.

--- test/spatial/test_frame.py
+++ test/spatial/test_frame.py
@@ -5,6 +5,9 @@ from robot.spatial import Dual, Frame, Quaternion, Transform, Vector3
 
 class TestFrame(unittest.TestCase):
   def setUp(self):
+    # Default Frame
+    self.f0 = Frame()
+
     # Frame is constructed by rotating around Z 45 degrees, rotation around new Y 135 degrees
     self.f1 = Frame(Dual(\
       Quaternion(0.353553, -0.353553, 0.853553, 0.146447),\
@@ -13,6 +16,17 @@ class TestFrame(unittest.TestCase):
 
     self.t = Transform(axis = Vector3(0, 0, 1), angle = math.radians(45))
     self.f2 = Frame(self.t.dual)
+
+  def test_init(self):
+    # Default frame is expected to be a standard XYZ coordinate frame
+    expected = Vector3(1, 0, 0)
+    self.assertEqual(self.f0.x(), expected)
+
+    expected = Vector3(0, 1, 0)
+    self.assertEqual(self.f0.y(), expected)
+
+    expected = Vector3(0, 0, 1)
+    self.assertEqual(self.f0.z(), expected)
 
   def test_euler(self):
     result = self.f1.euler()
