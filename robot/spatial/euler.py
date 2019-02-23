40f48b12af9476b105408d92ccfa270e9cb2b54e
James Schwartz <jbschwartz@users.noreply.github.com>
2019-02-23 18:15:38 -0500

    Implement conversion from quaternions to intrinsic euler angles.
    Eventually this will replace the code in the Frame class. Includes tests.

---
+++ robot/spatial/euler.py
@@ -0,0 +1,50 @@
+import math
+
+# All of these functions convert quaternion representations to intrinsic euler angles
+# This is done by converting the quaternion representation to a partial matrix representation and then to an euler angle representation
+# There are two solutions in general (unless the representation is singular, i.e. gimbal lock)
+
+# TODO: Need to handle singular cases
+
+def zyz(r, x, y, z):
+  xz = 2 * x * z
+  ry = 2 * r * y
+  yz = 2 * y * z
+  rx = 2 * r * x
+
+  beta = math.acos(1 - 2 * (x ** 2 + y ** 2))
+
+  results = []
+  for yp in [beta, -beta]:
+    sign = 1 if math.sin(yp) > 0 else -1
+    
+    zpp = math.atan2((yz + rx) * sign, (-xz + ry) * sign)
+    z = math.atan2((yz - rx) * sign, (xz + ry) * sign)
+    results.append([ z, yp, zpp ])
+
+  return results
+
+def zyx(r, x, y, z):
+  xy = 2 * x * y
+  xz = 2 * x * z
+  ry = 2 * r * y
+  yz = 2 * y * z
+  rx = 2 * r * x
+  rz = 2 * r * z
+
+  xSq = x ** 2
+  ySq = y ** 2
+  zSq = z ** 2
+
+  beta = math.asin(-xz + ry)
+
+  results = []
+  for yp in [beta, math.pi + beta]:
+    sign = 1 if math.cos(yp) > 0 else -1
+    
+    xpp = math.atan2((yz + rx) * sign, (1 - 2 * (xSq + ySq)) * sign)
+    z = math.atan2((xy + rz) * sign, (1 - 2 * (ySq + zSq)) * sign)
+
+    results.append([ z, yp, xpp ])
+
+  return results
---
+++ test/spatial/test_euler.py
@@ -0,0 +1,32 @@
+import unittest
+import math
+
+from operator import itemgetter
+
+from robot.spatial import euler, Quaternion
+
+class TestEuler(unittest.TestCase):
+  def setUp(self):
+    # Frame is constructed by rotating around Z 45 degrees, rotation around new Y 135 degrees 
+    self.q = Quaternion(0.353553, -0.353553, 0.853553, 0.146447)
+
+  def checkSolutions(self, results, solutions):
+    results.sort(key = itemgetter(1), reverse = True)
+    solutions.sort(key = itemgetter(1), reverse = True)
+
+    for index, (result, expecteds) in enumerate(zip(results, solutions)):
+      for angleIndex, (angle, expected) in enumerate(zip(result, expecteds)):
+        with self.subTest(f"Solution #{index+1}, Angle #{angleIndex+1}"):
+          self.assertAlmostEqual(angle, expected, places=5)
+
+  def test_zyx(self):
+    results = euler.zyx(*self.q)
+    solutions = [ [math.radians(-135), math.radians(45), math.radians(180)], [math.radians(45), math.radians(225), math.radians(0)] ]
+
+    self.checkSolutions(results, solutions)
+
+  def test_zyz(self):
+    results = euler.zyz(*self.q)
+    solutions = [ [math.radians(45), math.radians(135), math.radians(0)], [math.radians(-135), math.radians(-135), math.radians(-180)] ]
+
+    self.checkSolutions(results, solutions)
