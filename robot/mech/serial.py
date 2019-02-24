4223579b72c54ae251e6807d300d89c13f4dc037
James Schwartz <jbschwartz@users.noreply.github.com>
2019-02-24 13:16:32 -0500

    Serial class and tests.

---
+++ robot/mech/serial.py
@@ -0,0 +1,93 @@
+import math
+import copy
+import itertools
+
+from ..spatial import Dual, Quaternion, Transform, Frame, Vector3
+from robot import constant
+
+class Serial:
+  def __init__(self, joints : list):
+    self.joints = joints
+    self.position = Vector3()
+
+    self.checkStructure()
+
+  def checkStructure(self):
+    # TODO: Check the structure of the robot to see if it is 6R with spherical wrist
+    #   The code currently assumes this configuration only
+
+    # Robots handled in this code _must_ have a shoulder that is perpendicular to the waist
+    assert math.isclose(abs(self.joints[0].dh['alpha']), math.pi / 2), 'Robot does not have a recognized shoulder configuration'
+    
+    # The elbow must be parallel to the shoulder joint (alpha dictates the angle between z-axes)
+    correctElbow = math.isclose(self.joints[1].dh['alpha'], 0) or math.isclose(self.joints[1].dh['alpha'], math.pi)
+    assert correctElbow, 'Robot does not have a recognized elbow configuration'
+
+  def pose(self, qs : list = []) -> Frame: 
+    t = Transform()
+
+    for joint, value in itertools.zip_longest(self.joints, qs, fillvalue=0):
+      t *= joint.transform(value)
+
+    return Frame(t)
+
+  def poses(self, qs : list = []) -> list: 
+    ts = [ Transform() ]
+    
+    for joint, value in itertools.zip_longest(self.joints, qs, fillvalue=0):
+      # Get last frame
+      t = copy.deepcopy(ts[-1])
+      t *= joint.transform(value)
+      ts.append(t)
+    
+    return [ Frame(t) for t in ts ]
+
+  def upper_arm_length(self):
+    return self.joints[1].dh['a']
+
+  def fore_arm_length(self):
+    y = self.joints[2].dh['a']
+    x = self.joints[3].dh['d']
+    return math.sqrt(y ** 2 + x ** 2)
+
+  def shoulder_wrist_offset(self):
+    return self.joints[1].dh['d'] + self.joints[2].dh['d']
+
+  def shoulder_z(self):
+    return self.joints[0].dh['d']
+
+  def within_limits(self, qs : list):
+    for q, joint in zip(qs, self.joints):
+      if q != constant.SINGULAR:
+        if q < joint.limits['low'] or q > joint.limits['high']:
+          return False
+    return True
+
+  def transform_to_robot(self, angle_sets):
+    '''
+    Takes generic joint angles and transforms them to this specific robot's geometry
+    '''
+
+    zero = {
+      'waist': self.joints[0].dh['theta'],
+      'shoulder': self.joints[1].dh['theta'],
+      'elbow': math.atan(self.joints[3].dh['d'] / self.joints[2].dh['a'])
+    }
+
+    direction = {
+      'shoulder': 1 if self.joints[0].dh['alpha'] > 0 else -1
+    }
+    direction['elbow'] = -direction['shoulder'] if self.joints[1].dh['alpha'] == math.pi else direction['shoulder']
+
+    for angles in angle_sets:
+      angles[0] -= zero['waist']
+      angles[1] = direction['shoulder'] * angles[1] - zero['shoulder']
+      angles[2] = direction['elbow'] * (angles[2] + zero['elbow'])
+
+  def wrist_center(self, pose : Frame):
+    '''
+    Get wrist center point given the end-effector pose
+    '''
+    # TODO: Handle tools
+    wrist_length = self.joints[5].dh['d']
+    return pose.position() - pose.z() * wrist_length
---
+++ test/mech/test_serial.py
@@ -0,0 +1,61 @@
+import unittest
+import math
+
+from robot.spatial import Vector3
+from robot.mech import Joint, Serial
+
+from ..robots import abb_irb_120
+
+class TestSerial(unittest.TestCase):
+  def setUp(self):
+    self.robot = abb_irb_120.robot
+    
+  def test_pose(self):
+    angles = [ math.radians(45) ] * 6
+    frame = self.robot.pose(angles)
+    
+    result = frame.position()
+    expected = Vector3(133.58578643, 184.49747468, 128.00714267)
+
+    self.assertAlmostEqual(result, expected)
+
+  def test_poses(self):
+    angles = [ math.radians(0) ] * 6
+    frames = self.robot.poses(angles)
+    expecteds = [
+      Vector3(0, 0, 0),
+      Vector3(0, 0, 290),
+      Vector3(0, 0, 560),
+      Vector3(0, 0, 630),
+      Vector3(302, 0, 630),
+      Vector3(302, 0, 630),
+      Vector3(374, 0, 630)
+    ]
+    
+    for index, (frame, expected) in enumerate(zip(frames, expecteds)):
+      with self.subTest(f"Frame #{index + 1}"):
+        result = frame.position()
+        self.assertAlmostEqual(result, expected)
+
+  def test_transform_to_robot(self):
+    # The last argument accounts for the elbow offset to the wrist
+    results = [[ math.radians(45), 0, math.atan(70 / 302) ]]
+    solutions = [[ math.radians(45), math.radians(90), math.radians(-90) ]]
+    
+    self.robot.transform_to_robot(results)
+
+    for result, expecteds in zip(results, solutions):
+      for angle, expected in zip(result, expecteds):
+        self.assertAlmostEqual(angle, expected)
+
+  def test_wrist_center(self):
+    angles = [ math.radians(45) ] * 6
+    frame = self.robot.pose(angles)
+
+    result = self.robot.wrist_center(frame)
+    expected = Vector3(184.49747468, 184.49747468, 178.91883092)
+    
+    self.assertAlmostEqual(result, expected)
+
+
+
