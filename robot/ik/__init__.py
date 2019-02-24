470dce390abbd6d1e61fbc77b9cc6b837b6361a7
James Schwartz <jbschwartz@users.noreply.github.com>
2019-02-24 13:37:34 -0500

    solve_wrist function and test stubs.

--- robot/ik/__init__.py
+++ robot/ik/__init__.py
@@ -2,3 +2,4 @@ from .waist import solveWaist
 from .elbow import solveElbow
 from .shoulder import solveShoulder
 from .arm import solveArm
+from .wrist import solve_wrist
---
+++ test/ik/test_wrist.py
@@ -0,0 +1,13 @@
+import unittest
+
+from robot import ik
+
+class TestWrist(unittest.TestCase):
+  def setUp(self):
+    pass
+
+  @unittest.skip("Need to write test")
+  def test_wrist(self):
+    # Need to write this test with mocks for robot 
+    #   Otherwise I think it's testing too many other parts of the code
+    self.assertEqual(True, False)
