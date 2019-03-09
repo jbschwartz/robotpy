332f87cac354f21af181c463b932f41433ccb170
James Schwartz <jbschwartz@users.noreply.github.com>
2019-03-09 13:30:28 -0500

    Cache the value of Facet.computed_normal so as not to recalculate.
    Once the facet is defined with three vertices, we don't expect anything to change.

--- robot/visual/facet.py
+++ robot/visual/facet.py
@@ -7,18 +7,19 @@ Vector3 = vector3.Vector3
 class Facet:
   def __init__(self):
     self.passed_normal = Vector3()
+    self.computed_normal = None
     self.vertices = []
 
   def is_complete(self):
     return len(self.vertices) == 3
 
   def has_conflicting_normal(self):
-    # TODO: Probably need to fix this later: make math.isclose work on Vector3
-    difference = self.passed_normal - self.computed_normal()
+    if not self.computed_normal:
+      self.compute_normal()
 
-    return not math.isclose(difference.length(), 0.0)
+    return not vector3.almost_equal(self.passed_normal, self.computed_normal)
 
-  def computed_normal(self):
+  def compute_normal(self):
     if self.is_complete():
       edges = [ self.vertices[1] - self.vertices[0], self.vertices[2] - self.vertices[1]]
       normal = vector3.cross(edges[0], edges[1])
@@ -28,7 +29,7 @@ class Facet:
       
       normal.normalize()
 
-      return normal 
+      self.computed_normal = normal 
     else:
       # TODO: Figure out what an appropriate action would be here
       pass
