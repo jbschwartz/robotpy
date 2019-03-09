6f2d82316b3e0087998f08a732ee835ae31e0672
James Schwartz <jbschwartz@users.noreply.github.com>
2019-03-09 13:47:33 -0500

    Implement Mesh.add_facet function.
    Do naive vertex duplicate checking for the time being. This will be a problem for larger meshes.

--- robot/visual/mesh.py
+++ robot/visual/mesh.py
@@ -1,5 +1,7 @@
 from .facet import Facet 
 
+from ..spatial import vector3
+
 class Mesh:
   def __init__(self, name):
     self.name = name
@@ -11,5 +13,21 @@ class Mesh:
   def set_color(self, r, g, b):
     pass
 
+  def check_duplicate(self, new_vertex):
+    for index, stored_vertex in enumerate(self.vertices):
+      if vector3.almost_equal(stored_vertex, new_vertex):
+        return index
+    
+    return -1
+
   def add_facet(self, facet : Facet):
-    pass
+    # TODO: Spatial hashing to remove duplicates!
+    # TODO: Assert that the facet has three verticies
+    for new_vertex in facet.vertices:
+      duplicate_index = self.check_duplicate(new_vertex)
+      if duplicate_index >= 0:
+        self.indices.append(duplicate_index)
+      else:
+        self.vertices.append(new_vertex)
+        self.vertex_normals.append(facet.computed_normal)
+        self.indices.append(len(self.vertices) - 1)
