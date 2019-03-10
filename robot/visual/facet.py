5403a31347f959613088894beaf46f9c638925f0
James Schwartz <jbschwartz@users.noreply.github.com>
2019-03-10 17:59:31 -0400

    Begin generalizing Facet to handle more than three vertices.

--- robot/visual/facet.py
+++ robot/visual/facet.py
@@ -10,8 +10,11 @@ class Facet:
     self.computed_normal = None
     self.vertices = []
 
-  def is_complete(self):
-    return len(self.vertices) == 3
+  def is_triangle(self):
+    return self.size() == 3
+
+  def size(self):
+    return len(self.vertices)
 
   def has_conflicting_normal(self):
     if not self.computed_normal:
@@ -20,17 +23,16 @@ class Facet:
     return not vector3.almost_equal(self.passed_normal, self.computed_normal, 0.0001)
 
   def compute_normal(self):
-    if self.is_complete():
-      edges = [ self.vertices[1] - self.vertices[0], self.vertices[2] - self.vertices[1]]
-      normal = vector3.cross(edges[0], edges[1])
+    edges = [ self.vertices[1] - self.vertices[0], self.vertices[2] - self.vertices[1]]
+    normal = vector3.cross(edges[0], edges[1])
 
-      if math.isclose(normal.length(), 0.0):
-        # TODO: Probably should make a specific exception for this
-        raise Exception('Degenerate triangle found')
-      
-      normal.normalize()
+    if math.isclose(normal.length(), 0.0):
+      # TODO: Probably should make a specific exception for this
+      raise Exception('Degenerate triangle found')
+    
+    normal.normalize()
 
-      self.computed_normal = normal 
+    self.computed_normal = normal 
     else:
       # TODO: Figure out what an appropriate action would be here
       pass
