521a14da32c09fbde8903954588231a8977b7069
James Schwartz <jbschwartz@users.noreply.github.com>
2019-03-24 19:11:52 -0400

    Move robots folder to mech subpackage

---
+++ robot/mech/robots/__init__.py
@@ -0,0 +1,3 @@
+from .. import serial
+
+ABB_IRB_120 = serial.load('./robot/mech/robots/abb_irb_120.json')
--- robot/robots/__init__.py
+++
@@ -1,3 +0,0 @@
-from ..mech import serial
-
-ABB_IRB_120 = serial.load('./robot/robots/abb_irb_120.json')
