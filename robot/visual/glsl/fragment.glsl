fe7d2fdffeec2422d55332bfd65c813bbf70f3d2
James Schwartz <jbschwartz@users.noreply.github.com>
2019-03-30 12:37:26 -0400

    Update GLSL shaders to match changes made organizationally.

--- robot/visual/glsl/fragment.glsl
+++ robot/visual/glsl/fragment.glsl
@@ -6,33 +6,21 @@ in vec3 frag_pos;
 
 out vec4 fout_color;
 
+uniform vec3 light_position;
+uniform vec3 light_color;
+uniform float light_intensity;
+
 void main(void)
 {
-  vec3 lightPos = vec3(400, -400, 1200);
-  vec3 lightColor = vec3(1, 1, 1);
-
-  float ambientStrength = 0.3;
-  vec3 ambient = ambientStrength * lightColor;
+  vec3 ambient = light_intensity * light_color;
 
   vec3 norm = normalize(vout_normal);
-  vec3 lightDir = normalize(lightPos - frag_pos); 
+  vec3 lightDir = normalize(light_position - frag_pos); 
 
   float diff = max(dot(norm, lightDir), 0.1);
-  vec3 diffuse = diff * lightColor;
+  vec3 diffuse = diff * light_color;
 
-  vec3 color = vout_color;
-
-  // if (vout_usecoords == 1) {
-  //   vec4 tex_color = texture(ourTexture, texcoords);
-  //   if (tex_color.a < 1) {
-  //     color = vec3(1,1,1); 
-  //   } else {
-  //     color = vec3(0.55, 0.25, 0.95);//tex_color.xyz; 
-  //   }
-  // }
-  
-  vec3 result = (ambient + diffuse) * color;
-
+  vec3 result = (ambient + diffuse) * vout_color;
 
   fout_color = vec4(result, 1.0);
 }
