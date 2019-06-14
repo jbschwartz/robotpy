5432ef410b1dd27ef62fd0517e1e6a522f33ad42
James Schwartz <jbschwartz@users.noreply.github.com>
2019-06-13 20:53:20 -0400

    Store Frame in Link object

--- robot/mech/link.py
+++ robot/mech/link.py
@@ -1,6 +1,9 @@
+from robot.spatial.frame import Frame
+
 class Link:
   def __init__(self, name, color, mesh_file):
     # TODO: Mass, COM, Moments of Inertia
+    self.frame = Frame()
     self.name = name
     self.color = color
     self.mesh_file = mesh_file
