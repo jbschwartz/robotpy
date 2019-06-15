5d1f3c2df0a248e8c7e6d8c49f693e1caa24bf5f
James Schwartz <jbschwartz@users.noreply.github.com>
2019-06-15 13:47:58 -0400

    Remove some duplication.
    Between CameraController cursor and key orbit functions

--- robot/visual/camera_controller.py
+++ robot/visual/camera_controller.py
@@ -15,43 +15,35 @@ class CameraController(Observer):
     pass
   
   def cursor(self, cursor):
-    position = Vector3(*cursor.xy, 0)
-
     if self.last_cursor_position:
-      difference = (self.last_cursor_position - position).normalize()
-      
-      angle = Vector3()
-      angle.x = self.ORBIT_SPEED * difference.y 
-      angle.z = self.ORBIT_SPEED * difference.x 
-      
-      self.camera.orbit(angle.x, angle.z)
+      difference = (self.last_cursor_position - cursor).normalize()
+      self.request_orbit(difference.y, difference.x)
 
-    self.last_cursor_position = position
+    self.last_cursor_position = cursor
   
   def key(self, key, action):
     if key == glfw.KEY_R and action == glfw.RELEASE:
       self.camera.reset()
 
     if key in [glfw.KEY_RIGHT, glfw.KEY_LEFT, glfw.KEY_UP, glfw.KEY_DOWN]:
-      # TODO: This doesn't handle both keys pressed at once
-      self.request_orbit(key, action)
+      self.arrows(key, action)
 
-  def request_orbit(self, key, action):
-    angle = Vector3()
-    
+  def arrows(self, key, action):
+    # TODO: This doesn't handle both keys pressed at once
     direction = {
       glfw.KEY_RIGHT: 1,
       glfw.KEY_LEFT: -1,
       glfw.KEY_UP: -1,
       glfw.KEY_DOWN: 1
     }
-
+    
     if key in [glfw.KEY_RIGHT, glfw.KEY_LEFT] and action in [glfw.PRESS, glfw.REPEAT]:
-      angle.z = self.ORBIT_SPEED * direction[key]
+      self.request_orbit(0, direction[key])
     if key in [glfw.KEY_UP, glfw.KEY_DOWN] and action in [glfw.PRESS, glfw.REPEAT]:
-      angle.x = self.ORBIT_SPEED * direction[key]
+      self.request_orbit(direction[key], 0)
 
-    self.camera.orbit(angle.x, angle.z)
+  def request_orbit(self, x, z):
+    self.camera.orbit(self.ORBIT_SPEED * x, self.ORBIT_SPEED * z)
 
   def scroll(self, direction):
     pass
