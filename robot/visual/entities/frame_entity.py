1fd1aa59f88ce9e95f9a3e61eaf0ce77f4cc33a0
James Schwartz <jbschwartz@users.noreply.github.com>
2019-06-14 20:35:43 -0400

    Add entity for rendering a coordinate frame.

---
+++ robot/visual/entities/frame_entity.py
@@ -0,0 +1,89 @@
+import math
+import numpy as np
+
+from OpenGL.GL import *
+
+from ctypes import c_void_p
+
+from robot.spatial.frame          import Frame
+from robot.spatial.matrix4        import Matrix4
+from robot.spatial.transform      import Transform
+from robot.spatial.vector3        import Vector3
+from robot.visual.entities.entity import Entity
+from robot.visual.shader_program  import ShaderProgram
+
+class FrameEntity(Entity):
+  def __init__(self, shader_program : ShaderProgram = None, color = (0.5, 0, 0)):
+    self.buffer = []
+
+    self.scale = 25
+
+    Entity.__init__(self, shader_program, color)
+
+  def build_buffer(self):
+    # TODO: Something prettier maybe?
+    data_list = [
+       10, 0, 0.5,
+       0, 0,  0.5,
+       0, 0, -0.5,
+       0, 0, -0.5,
+       10, 0, -0.5,
+       10, 0, 0.5,
+
+       0.5,  10, 0,
+       0.5,  0, 0,
+      -0.5,  0, 0,
+      -0.5,  0, 0,
+      -0.5,  10, 0,
+       0.5, 10, 0,
+
+       0.5, 0,  10,
+       0.5, 0,  0,
+      -0.5, 0,  0,
+      -0.5, 0,  0,
+      -0.5, 0,  10,
+       0.5, 0, 10
+    ]
+    
+    self.buffer = np.array([self.scale * component for component in data_list], dtype=np.float32)
+
+  def load(self):
+    self.build_buffer()
+
+    glBindVertexArray(self.vao)
+
+    glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
+    glBufferData(GL_ARRAY_BUFFER, self.buffer.nbytes, self.buffer, GL_STATIC_DRAW)
+
+    glVertexAttribPointer(self.shader_program.attribute_location('vin_position'), 3, GL_FLOAT, GL_FALSE, 12, None)
+    glEnableVertexAttribArray(self.shader_program.attribute_location('vin_position'))
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0)
+    glBindVertexArray(0)
+
+  def update(self, delta):
+    pass
+
+  def draw(self, camera, light, transform):
+    glUseProgram(self.shader_program.program_id)
+
+    self.shader_program.proj_matrix = camera.projection
+    self.shader_program.view_matrix = camera.world_to_camera
+
+    self.shader_program.light_position  = light.position
+    self.shader_program.light_color     = light.color
+    self.shader_program.light_intensity = light.intensity
+    
+    self.shader_program.model_matrix = transform
+
+    glBindVertexArray(self.vao)
+
+    # TODO: This is a very hacky way to handle coloring
+    self.shader_program.color_in     = Vector3(0.5, 0, 0)
+    glDrawArrays(GL_TRIANGLES, 0, 6)
+    self.shader_program.color_in     = Vector3(0, 0.5, 0)
+    glDrawArrays(GL_TRIANGLES, 6, 6)
+    self.shader_program.color_in     = Vector3(0, 0, 0.5)
+    glDrawArrays(GL_TRIANGLES, 12, 6)
+
+    glBindVertexArray(0)
