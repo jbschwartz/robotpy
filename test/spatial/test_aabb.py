1edddbb568012e62e9ac00db71855d536e18b819
James Schwartz <jbschwartz@users.noreply.github.com>
2019-06-18 20:31:12 -0400

    Add test for extending an AABB with another AABB.
    e.g. for finding the bounding box of bounding boxes.

--- test/spatial/test_aabb.py
+++ test/spatial/test_aabb.py
@@ -11,8 +11,17 @@ class TestAABB(unittest.TestCase):
     self.v3 = Vector3(0, 1, 2)
     self.v4 = Vector3(4, 4, 4)
 
-  def test_extend(self):
     self.aabb.extend(self.v1, self.v2, self.v3)
 
+  def test_extend(self):
     self.assertAlmostEqual(self.v2, self.aabb.min)
     self.assertAlmostEqual(self.v1, self.aabb.max)
+
+  def test_extend_aabb(self):
+    other_aabb = AABB()
+    other_aabb.extend(self.v3, self.v4)
+
+    self.aabb.extend(other_aabb)
+    
+    self.assertAlmostEqual(self.v2, self.aabb.min)
+    self.assertAlmostEqual(self.v4, self.aabb.max)
