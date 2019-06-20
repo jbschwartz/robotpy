9f9f25137480959e88093fa489072036d78e029a
James Schwartz <jbschwartz@users.noreply.github.com>
2019-06-19 20:25:54 -0400

    Rework Link to store its mesh.
    Also calculate its AABB given its local mesh AABB and its current transformation.

--- robot/mech/link.py
+++ robot/mech/link.py
@@ -1,9 +1,17 @@
+from robot.spatial.aabb  import AABB
 from robot.spatial.frame import Frame
 
 class Link:
-  def __init__(self, name, color, mesh_file):
+  def __init__(self, name, mesh, color):
     # TODO: Mass, COM, Moments of Inertia
     self.frame = Frame()
     self.name = name
+    self.mesh = mesh
     self.color = color
-    self.mesh_file = mesh_file
+
+  @property
+  def aabb(self):
+    aabb = AABB()
+    for corner in self.mesh.aabb.corners:
+      aabb.extend(self.frame.transform(corner))
+    return aabb
