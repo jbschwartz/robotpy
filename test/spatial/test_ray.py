628768b047d7c252038e68d6a4dd24f27e34e96c
James Schwartz <jbschwartz@users.noreply.github.com>
2019-07-03 21:09:20 -0400

    Test Ray.init and Ray.evaluate.
    Further test cases are necessary.

---
+++ test/spatial/test_ray.py
@@ -0,0 +1,24 @@
+import math, unittest
+
+from robot.spatial.ray     import Ray
+from robot.spatial         import vector3
+
+Vector3 = vector3.Vector3
+
+class TestRay(unittest.TestCase):
+  def setUp(self):
+    self.direction = Vector3(-1, 4, 5)
+    self.ray = Ray(Vector3(1, 2, 3), self.direction)
+    
+  def test_init(self):
+    result = self.ray.direction
+    expected = vector3.normalize(self.direction)
+
+    self.assertAlmostEqual(result, expected)
+
+  def test_evaluate(self):
+    # TODO: Needs more testing
+    result = self.ray.evaluate(0)
+    expected = self.ray.origin
+
+    self.assertAlmostEqual(result, expected)
