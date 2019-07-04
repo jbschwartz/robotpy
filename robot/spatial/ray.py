d8dee89f92e0a7cd193b8c2169aa503212c0a3da
James Schwartz <jbschwartz@users.noreply.github.com>
2019-07-03 21:08:55 -0400

    Normalize Ray.direction.
    Also provide an evaluate function to get the location along a ray given a parameter t.

--- robot/spatial/ray.py
+++ robot/spatial/ray.py
@@ -1,7 +1,12 @@
 class Ray():
   def __init__(self, origin, direction):
     self.origin = origin
-    self.direction = direction
+    # TODO: What about a zero-length direction vector?
+    self.direction = direction.normalize()
+
+  def evaluate(self, t):
+    '''Return the location along ray given parameter t.'''
+    return self.origin + t * self.direction
 
   def __str__(self):
     return f"{self.origin} + t * {self.direction}"
