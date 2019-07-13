c4267adc9a35e2a72f8f91e12af31fd1e4147147
James Schwartz <jbschwartz@users.noreply.github.com>
2019-07-13 12:39:29 -0400

    Finish up KDTreeNode comments.

--- robot/spatial/kdtree.py
+++ robot/spatial/kdtree.py
@@ -7,49 +7,58 @@ class KDTreeNode():
     self.children = []
 
   def is_leaf(self):
+    '''Return True if the KDTreeNode is a leaf node.''' 
     return len(self.children) == 0
 
   def can_branch(self, depth):
+    '''Return True if the KDTreeNode can be split into two child branches.'''
+    # TODO: Maybe use a more sophisticated cost function to evaluate whether we should branch
     return len(self.facets) > 0 and depth < DEPTH_BOUND
 
   def splitting_plane(self, depth):
+    '''Get the splitting plane (i.e. axis and value) for the given depth.'''
+    # TODO: Maybe use a more sophisticated splitting plane generation like SAH.
     axis = depth % 3
     plane_value = self.aabb.center[axis]
 
     return axis, plane_value
 
-  def split_facets(self, axis, split_plane):
-    '''Split the list of facets in the node into left and right lists based on splitting axis and plane'''
+  def split_facets(self, axis, plane_value):
+    '''Split the list of facets in the node into left and right lists based on splitting plane.'''
     left, right = [], []
 
     for facet in self.facets:
       # Check minimum bound on left and maximum bound on right so that the triangle is not included in any list twice
       # This covers both scenarios: facets belonging to one side only or both sides
-      if facet.aabb.min[axis] < split_plane:
+      if facet.aabb.min[axis] < plane_value:
         left.append(facet)
-      if facet.aabb.max[axis] > split_plane:
+      if facet.aabb.max[axis] > plane_value:
         right.append(facet)
     
     return left, right
 
   def candidate_children(self, splitting_plane):
+    '''Get child AABBs and facet lists from a given splitting plane.'''
     child_boxes = self.aabb.split(*splitting_plane)
     child_facets = self.split_facets(*splitting_plane)
 
     return zip(child_boxes, child_facets)
 
   def branch(self, depth = 0):
+    '''If possible, split the current KDTreeNode into two children nodes.'''
     if not self.can_branch(depth):
       return
 
     splitting_plane = self.splitting_plane(depth)
 
-    for aabb, facets in self.candidate_children(splitting_plane):
-      node = KDTreeNode(aabb, facets)
+    for child in self.candidate_children(splitting_plane):
+      node = KDTreeNode(*child)
       node.branch(depth + 1)
 
       self.children.append(node)
 
+    # Interrior nodes to the KDTree should not have any facets (only leaf nodes should).
+    # If we've gotten this far, this node is an interrior node
     self.facets = None
 
   def intersect(self, ray):
