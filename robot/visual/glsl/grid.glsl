e09c52d5e16da423669c5006df570b8f1fb9aabd
James Schwartz <jbschwartz@users.noreply.github.com>
2019-07-14 12:46:02 -0400

    Rename and combine shaders.

--- robot/visual/glsl/grid.glsl
+++ robot/visual/glsl/grid.glsl
@@ -1,3 +1,21 @@
+#if defined(VERTEX)
+
+in vec3 vin_position;
+out vec4 vout_world_pos;
+out vec4 gl_Position;
+
+uniform mat4 proj_matrix;
+uniform mat4 view_matrix;
+uniform mat4 scale_matrix;
+
+void main()
+{
+  vout_world_pos = scale_matrix * vec4(vin_position, 1.0);
+  gl_Position = proj_matrix * view_matrix * vout_world_pos;
+}
+
+#elif defined(FRAGMENT)
+
 in vec4 vout_world_pos;
 out vec4 gl_FragColor;
 
@@ -39,3 +57,5 @@ void main(void)
   gl_FragColor.a = gl_FragColor.a - smoothstep(2000, 4000, length(vout_world_pos.xy));
 
 }
+
+#endif
