9c4d5fb1dc1b27761d5b02a93e9fc35f1f5f02a6
James Schwartz <jbschwartz@users.noreply.github.com>
2019-07-20 13:08:58 -0400

    Whitespace and comments.

--- robot/spatial/transform.py
+++ robot/spatial/transform.py
@@ -8,22 +8,19 @@ Quaternion = quaternion.Quaternion
 Dual       = dual.Dual
 
 class Transform:
-  '''
-  Class representing spatial rigid body transformation in three dimensions
-  '''
+  '''Spatial rigid body transformation in three dimensions.'''
   def __init__(self, dual: Dual = None):
     self.dual = dual or Dual(Quaternion(1, 0, 0, 0), Quaternion(0, 0, 0, 0))
 
   @classmethod
   def from_axis_angle_translation(cls, axis = Vector3(), angle = 0, translation = Vector3()):
+    '''Create a Transformation from axis, angle, and translation components.'''
     r = Quaternion.from_axis_angle(axis, angle)
     t = Quaternion(0, *translation)
     return cls(Dual(r, 0.5 * t * r))
 
   def __mul__(self, other):
-    '''
-    Composition of transformations
-    '''
+    '''Compose this Transformation with another Transformation.'''
     if isinstance(other, Transform):
       return Transform(self.dual * other.dual)
     else:
@@ -31,16 +28,22 @@ class Transform:
 
   __rmul__ = __mul__
 
-  def __call__(self, other, as_type="point"):
-    if isinstance(other, (list, tuple)):
-      return [self.__call__(item) for item in other]
+  def __call__(self, vector: Vector3, as_type="point"):
+    '''Apply Transformation to a Vector3 with call syntax.'''
+    if isinstance(vector, (list, tuple)):
+      return [self.__call__(item) for item in vector]
 
-    if not isinstance(other, Vector3):
+    if not isinstance(vector, Vector3):
       raise NotImplementedError
 
-    return self.transform(other, as_type)
+    return self.transform(vector, as_type)
 
-  def transform(self, vector, as_type):
+  def transform(self, vector: Vector3, as_type) -> Vector3:
+    '''
+    Apply the transform to the provided Vector3.
+
+    Optionally treat the Vector3 as a point and apply a transformation to its position.
+    '''
     q = Quaternion(0, *vector.xyz)
     if as_type == 'vector':
       d = Dual(q, Quaternion(0, 0, 0, 0))
@@ -54,17 +57,17 @@ class Transform:
       raise KeyError
 
   def translation(self) -> Vector3:
+    '''Return the transform's translation Vector3.'''
     # "Undo" what was done in the __init__ function by working backwards
     t = 2 * self.dual.d * quaternion.conjugate(self.dual.r)
     return Vector3(*t.xyz)
 
   def rotation(self) -> Quaternion:
+    '''Return the transform's rotation Quaternion.'''
     return self.dual.r
 
-  def inverse(self):
-    '''
-    Return a new Transformation that is an inverse to this transformation
-    '''
+  def inverse(self) -> 'Transform':
+    '''Return a new inverse Transformation.'''
     rstar = quaternion.conjugate(self.dual.r)
     dstar = quaternion.conjugate(self.dual.d)
 
