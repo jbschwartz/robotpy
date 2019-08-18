2e9823276d3bc5477d492aafcabc579670e210c8
James Schwartz <jbschwartz@users.noreply.github.com>
2019-08-18 13:37:24 -0400

    Work on Uniform setter decorators.
    Better type and size checking. TODO: Use array_size to check the number of elements passed to setter.

--- robot/visual/opengl/uniform.py
+++ robot/visual/opengl/uniform.py
@@ -1,28 +1,34 @@
 import numpy as np
 
-from typing import Callable
+from numbers import Number
+from typing  import Callable, Iterable, Union
 
 from OpenGL.GL import *
 
+from robot.common  import logger
 from robot.spatial import Matrix4, Transform, Vector3
 
-def vector_decorator(gl_function):
-  def wrapper(location, vectors):
-    # TODO: Better type/size checking
-    # This entire function is actually broken since there are glUniform[1,2,3,4]x functions
-    # This assumes it's always 3
-    if isinstance(vectors, Vector3):
-      return gl_function(location, 1, [*vectors])
-    elif isinstance(vectors, (list, tuple)):
-      if not any(map(lambda v: isinstance(v, (Vector3, list, tuple)), vectors)):
-        return gl_function(location, 1, [*vectors])
+def vector_decorator(gl_function: int, vector_size: int) -> Callable:
+  # TODO: Implement glUniform[1,2,4]xv functions
+  assert vector_size == 3, "Only vectors of size 3 are implemented"
 
-    assert isinstance(vectors, (list, tuple))
-    assert all(map(lambda v: isinstance(v, (Vector3, list, tuple)), vectors))
+  def wrapper(location: int, values: Union[Vector3, Iterable]):
+    if isinstance(values, Vector3):
+      return gl_function(location, 1, values.xyz)
+    elif isinstance(values, (list, tuple)):
+      # See if we have a list of "vectors" or a list of numerical values
+      if all([isinstance(v, (Vector3, list, tuple)) for v in values]):
+        assert all(len(iterable) == vector_size for iterable in values), "All elements must be of size {vector_size}"
+        # OpenGL expects a flat list of vector components
+        components = [component for v in values for component in v]
+        assert all([isinstance(component, Number) for component in components]), "All components must be numeric"
 
-    # OpenGL expects a flat list of vector components
-    components = [comp for v in vectors for comp in v]
-    return gl_function(location, len(vectors), components)
+        return gl_function(location, len(values), components)
+      elif all([isinstance(v, Number) for v in values]):
+        assert len(values) == vector_size
+        return gl_function(location, 1, values)
+    else:
+      raise TypeError(f'Unknown type {type(vectors)} given to Uniform setter')
 
   return wrapper
 
@@ -51,13 +57,16 @@ def matrix_decorator(gl_function):
   return wrapper
 
 GL_TYPE_UNIFORM_FN = {
-  GL_INT:        glUniform1iv,
+  GL_INT:        glUniform1i,
   GL_FLOAT:      glUniform1f,
   GL_BOOL:       glUniform1i,
-  GL_FLOAT_VEC3: vector_decorator(glUniform3fv),
+  GL_FLOAT_VEC3: vector_decorator(glUniform3fv, 3),
   GL_FLOAT_MAT4: matrix_decorator(glUniformMatrix4fv),
   GL_SAMPLER_2D: None
 }
+
+def setter_factory(gl_type: int, array_size: int) -> Callable:
+  return GL_TYPE_UNIFORM_FN[gl_type]
 
 class Uniform:
   def __init__(self, name: str, location: int, set_function: Callable) -> None:
@@ -69,9 +78,9 @@ class Uniform:
   @classmethod
   def from_program_index(cls, program_id: int, index: int) -> 'Uniform':
     """Construct a Uniform from the shader program id and the Uniform index."""
-    properties = [GL_TYPE, GL_NAME_LENGTH, GL_LOCATION]
+    properties = [GL_TYPE, GL_NAME_LENGTH, GL_LOCATION, GL_ARRAY_SIZE]
 
-    gl_type, name_length, location = glGetProgramResourceiv(
+    gl_type, name_length, location, array_size = glGetProgramResourceiv(
       program_id,
       GL_UNIFORM,
       index,
@@ -87,7 +96,7 @@ class Uniform:
     name = ''.join(chr(c) for c in name_ascii).strip('\x00').strip('[0]')
 
     try:
-      set_function = GL_TYPE_UNIFORM_FN[gl_type]
+      set_function = setter_factory(gl_type, array_size)
     except KeyError:
       raise KeyError(f'For {name}, unknown uniform type: {gl_type}')
 
@@ -98,6 +107,9 @@ class Uniform:
     return self._value
 
   @value.setter
-  def value(self, *args):
-    self._value = args
-    self.set_function(self.location, *args)
+  def value(self, value):
+    self._value = value
+    try:
+      self.set_function(self.location, value)
+    except TypeError:
+      logger.error(f'When setting Uniform {self.name}, unknown type {type(value)} given.')
