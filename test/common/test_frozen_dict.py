4de318e9595a568db21a6bdd1ff1e1335f4aa770
James Schwartz <jbschwartz@users.noreply.github.com>
2019-08-21 19:45:59 -0400

    FrozenDict class.
    A FrozenDict allows a predetermined set of keys to be read and written. Trying to create or update any keys not provided at initialization results in an AttributeError.

---
+++ test/common/test_frozen_dict.py
@@ -0,0 +1,34 @@
+import unittest
+
+from robot.common import FrozenDict
+
+class TestFrozenDict(unittest.TestCase):
+  def setUp(self):
+    self.underlying = {
+      'a': 1,
+      'b': 2
+    }
+
+    self.fd = FrozenDict(self.underlying)
+
+  def test_frozen_dict_accesses_with_dot_notation(self):
+    for key, value in self.underlying.items():
+      with self.subTest(f'Dictionary key {key}'):
+        self.assertEquals(getattr(self.fd, key), value)
+
+  def test_frozen_dict_sets_valid_keys(self):
+    for key, value in self.underlying.items():
+      new_value = value ** 2
+      setattr(self.fd, key, new_value)
+      self.assertEquals(getattr(self.fd, key), new_value)
+
+  def test_frozen_dict_raises_getting_invalid_keys(self):
+    with self.assertRaises(AttributeError):
+      getattr(self.fd, 'this_key_doesnt_exist')
+
+  def test_frozen_dict_raises_setting_invalid_keys(self):
+    with self.assertRaises(AttributeError):
+      self.fd.this_key_doesnt_exist = 2
+
+    with self.assertRaises(AttributeError):
+      getattr(self.fd, 'this_key_doesnt_exist')
