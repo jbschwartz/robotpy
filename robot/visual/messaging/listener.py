1a2b5e8a5b8035f9a46fb10a634d823b8f5e2e62
James Schwartz <jbschwartz@users.noreply.github.com>
2019-08-24 23:36:15 -0400

    Fix bug with Listener wrapper.
    Attributes being set were being stored on the wrapper and pulled from the wrapped so things were out of sync. TODO: Make the wrapper more transparent as in functools wraps.

--- robot/visual/messaging/listener.py
+++ robot/visual/messaging/listener.py
@@ -12,23 +12,20 @@ def listen(*events: Event) -> Callable:
 
 def listener(cls):
   """Listener class decorator registers all event handling methods."""
-  class ListenerWrapper:
+  class ListenerWrapper(cls):
     def __init__(self, *args, **kwargs) -> None:
-      self.wrapped = cls(*args, **kwargs)
       self._register_listeners()
+      super().__init__(*args, **kwargs)
 
     def _register_listeners(self) -> None:
       """Add methods with `_listen_to` attribute to the Event listener registry."""
       for name, method in cls.__dict__.items():
         if getattr(method, '_listen_to', None):
-          listener = getattr(self.wrapped, name)
+          listener = getattr(self, name)
           for event in method._listen_to:
             if registry.get(event):
               registry[event].append(listener)
             else:
               registry[event] = [listener]
-
-    def __getattr__(self, attr):
-      return getattr(self.wrapped, attr)
 
   return ListenerWrapper
