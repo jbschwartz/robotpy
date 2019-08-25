2052701d9ceaa50c30e35bf77b47d202f089ec47
James Schwartz <jbschwartz@users.noreply.github.com>
2019-08-25 13:36:41 -0400

    First pass at Buffer class.
    This will be offloading all of the VAO/VBO configuration from the Entity classes. This will also allow
    us to share Buffer objects across multiple entities.

---
+++ robot/visual/opengl/buffer.py
@@ -0,0 +1,132 @@
+import enum
+
+import numpy as np
+
+from ctypes import c_void_p
+from typing import Iterable
+
+from robot.common    import logger
+from robot.spatial   import Mesh
+from .shader_program import ShaderProgram
+
+from OpenGL.GL import *
+
+MESH_BUFFER_ATTRS = {
+  'position':   {'type': GL_FLOAT, 'number_of_components': 3},
+  'normal':     {'type': GL_FLOAT, 'number_of_components': 3},
+  'mesh_index': {'type': GL_INT,   'number_of_components': 1},
+}
+
+ATTRIBI_TYPES = (GL_BYTE, GL_UNSIGNED_BYTE, GL_SHORT, GL_UNSIGNED_SHORT, GL_INT, GL_UNSIGNED_INT)
+
+class Buffer():
+  """OpenGL Buffer instance."""
+  def __init__(self, data: np.array = None, attributes: dict = None, size: int = None) -> None:
+    self.vao = glGenVertexArrays(1)
+
+    if data is not None:
+      if len(data) == 0 or attributes is None:
+        return logger.warn(f"Buffer given no data or no vertex attributes to access it.")
+      elif size is not None:
+        logger.warn(f"Buffer size passed but ignored (since data is provided).")
+
+      self.vbo        = glGenBuffers(1)
+      self.data       = data
+      self.attributes = attributes
+    else:
+      if size is None:
+        return logger.warn(f"Buffer given no data, attributes, or size.")
+
+      self._size = size
+
+  def __len__(self) -> int:
+    """Return the number of elements in the buffer."""
+    if self.data is not None:
+      return len(self.data)
+    else:
+      return self._size
+
+  def __enter__(self) -> 'Buffer':
+    glBindVertexArray(self.vao)
+    return self
+
+  def __exit__(self, exc_type, exc_value, exc_traceback) -> None:
+    glBindVertexArray(0)
+
+  @classmethod
+  def from_mesh(cls, mesh: Mesh) -> 'Buffer':
+    """Create a Buffer from a Mesh."""
+    data = np.array(mesh.get_buffer_data(), dtype=[('', np.float32, 6),('', np.int32, 1)])
+    return cls(data, MESH_BUFFER_ATTRS)
+
+  @classmethod
+  def from_meshes(cls, meshes: Iterable[Mesh]) -> 'Buffer':
+    """Create one Buffer for a collection of Meshes."""
+    data = np.array([], dtype=[('', np.float32, 6),('', np.int32, 1)])
+    for mesh_index, mesh in enumerate(meshes):
+      mesh_data = mesh.get_buffer_data(mesh_index)
+      data = np.concatenate((data, mesh_data), axis=0)
+
+    return cls(data, MESH_BUFFER_ATTRS)
+
+  @classmethod
+  def Procedural(cls, size) -> 'Buffer':
+    """Create an empty buffer for 'procedural' shader programs."""
+    return cls(None, None, size)
+
+  @property
+  def stride(self) -> int:
+    # TODO: Do not assume that all buffer values are four bytes
+    return 4 * sum([
+      parameters['number_of_components']
+      for parameters in self.attributes.values()
+    ])
+
+  def set_attribute_locations(self, sp: ShaderProgram) -> None:
+    for attribute_name in self.attributes.keys():
+      try:
+        # TODO: This is probably _very_ bad. We shouldn't be mutating the MESH_BUFFER_ATTRS dictionary
+        self.attributes[attribute_name]['location'] = sp.attribute_location(f'vin_{attribute_name}')
+      except AttributeError:
+        logger.warn(f'Attribute `vin_{attribute_name}` not found in shader program `{sp.name}`')
+
+  def load(self) -> None:
+    assert all(
+      [
+        'location' in parameters
+        for parameters in self.attributes.values()
+      ]
+    ), "Buffer attribute locations must be set before the buffer can be loaded"
+
+    glBindVertexArray(self.vao)
+
+    glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
+    glBufferData(GL_ARRAY_BUFFER, self.data.nbytes, self.data, GL_STATIC_DRAW)
+
+    offset = 0
+    for parameters in self.attributes.values():
+      if parameters['type'] in ATTRIBI_TYPES:
+        glVertexAttribIPointer(
+          parameters['location'],
+          parameters['number_of_components'],
+          parameters['type'],
+          self.stride,
+          c_void_p(offset)
+        )
+      else:
+        glVertexAttribPointer(
+          parameters['location'],
+          parameters['number_of_components'],
+          parameters['type'],
+          GL_FALSE,
+          self.stride,
+          c_void_p(offset)
+        )
+
+      # TODO: Do not assume that all buffer values are four bytes
+      offset += 4 * parameters['number_of_components']
+
+      glEnableVertexAttribArray(parameters['location'])
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0)
+    glBindVertexArray(0)
