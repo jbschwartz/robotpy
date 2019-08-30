7ae5905552066268ea4dec546c9eda55bc31852f
James Schwartz <jbschwartz@users.noreply.github.com>
2019-08-29 20:36:57 -0400

    Create a frame STL file. Use STL file and buffers in FrameEntity.

--- robot/visual/entities/frame_entity.py
+++ robot/visual/entities/frame_entity.py
@@ -5,9 +5,11 @@ from OpenGL.GL import *
 
 from ctypes import c_void_p
 
-from robot.spatial                import Matrix4, Transform, Vector3
-from robot.visual.entities.entity import Entity
-from robot.visual.opengl.shader_program  import ShaderProgram
+from robot.spatial                         import Matrix4, Mesh, Transform, Vector3
+from robot.visual.entities.entity          import Entity
+from robot.visual.filetypes.stl.stl_parser import STLParser
+from robot.visual.opengl.buffer            import Buffer
+from robot.visual.opengl.shader_program    import ShaderProgram
 
 class FrameEntity(Entity):
   def __init__(self, frame = Transform(), shader_program : ShaderProgram = None):
@@ -17,181 +19,20 @@ class FrameEntity(Entity):
 
     Entity.__init__(self, shader_program)
 
-  def build_buffer(self):
-    data_list = [
-      # X axis
-       8.00, -0.25,  0.25,
-       0.25, -0.25,  0.25,
-       0.25, -0.25, -0.25,
-       0.25, -0.25, -0.25,
-       8.00, -0.25, -0.25,
-       8.00, -0.25,  0.25,
-
-       0.25,  0.25, -0.25,
-       0.25,  0.25,  0.25,
-       8.00,  0.25,  0.25,
-       8.00,  0.25,  0.25,
-       8.00,  0.25, -0.25,
-       0.25,  0.25, -0.25,
-
-       8.00,  0.25,  0.25,
-       0.25,  0.25,  0.25,
-       0.25, -0.25,  0.25,
-       0.25, -0.25,  0.25,
-       8.00, -0.25,  0.25,
-       8.00,  0.25,  0.25,
-
-       0.25, -0.25, -0.25,
-       0.25,  0.25, -0.25,
-       8.00,  0.25, -0.25,
-       8.00,  0.25, -0.25,
-       8.00, -0.25, -0.25,
-       0.25, -0.25, -0.25,
-
-       8.00,  0.25,  0.25,
-       8.00, -0.25,  0.25,
-       8.00, -0.25, -0.25,
-       8.00, -0.25, -0.25,
-       8.00,  0.25, -0.25,
-       8.00,  0.25,  0.25,
-
-       0.25, -0.25, -0.25,
-       0.25, -0.25,  0.25,
-       0.25,  0.25,  0.25,
-       0.25,  0.25,  0.25,
-       0.25,  0.25, -0.25,
-       0.25, -0.25, -0.25,
-
-      # Y axis
-      -0.25,  0.25,  0.25,
-       0.25,  0.25,  0.25,
-       0.25,  8.00,  0.25,
-       0.25,  8.00,  0.25,
-      -0.25,  8.00,  0.25,
-      -0.25,  0.25,  0.25,
-
-       0.25,  8.00, -0.25,
-       0.25,  0.25, -0.25,
-      -0.25,  0.25, -0.25,
-      -0.25,  0.25, -0.25,
-      -0.25,  8.00, -0.25,
-       0.25,  8.00, -0.25,
-
-       0.25,  0.25,  0.25,
-       0.25,  0.25, -0.25,
-       0.25,  8.00, -0.25,
-       0.25,  8.00, -0.25,
-       0.25,  8.00,  0.25,
-       0.25,  0.25,  0.25,
-
-      -0.25,  8.00, -0.25,
-      -0.25,  0.25, -0.25,
-      -0.25,  0.25,  0.25,
-      -0.25,  0.25,  0.25,
-      -0.25,  8.00,  0.25,
-      -0.25,  8.00, -0.25,
-
-      -0.25,  0.25, -0.25,
-       0.25,  0.25,  0.25,
-      -0.25,  0.25,  0.25,
-      -0.25,  0.25, -0.25,
-       0.25,  0.25, -0.25,
-       0.25,  0.25,  0.25,
-
-      -0.25,  8.00, -0.25,
-      -0.25,  8.00,  0.25,
-       0.25,  8.00,  0.25,
-       0.25,  8.00,  0.25,
-       0.25,  8.00, -0.25,
-      -0.25,  8.00, -0.25,
-
-      # Z axis
-       0.25,  0.25,  8.00,
-       0.25,  0.25,  0.25,
-      -0.25,  0.25,  0.25,
-      -0.25,  0.25,  0.25,
-      -0.25,  0.25,  8.00,
-       0.25,  0.25,  8.00,
-
-      -0.25, -0.25,  0.25,
-       0.25, -0.25,  0.25,
-       0.25, -0.25,  8.00,
-       0.25, -0.25,  8.00,
-      -0.25, -0.25,  8.00,
-      -0.25, -0.25,  0.25,
-
-       0.25, -0.25,  8.00,
-       0.25, -0.25,  0.25,
-       0.25,  0.25,  0.25,
-       0.25,  0.25,  0.25,
-       0.25,  0.25,  8.00,
-       0.25, -0.25,  8.00,
-
-      -0.25,  0.25,  8.00,
-      -0.25,  0.25,  0.25,
-      -0.25, -0.25,  0.25,
-      -0.25, -0.25,  0.25,
-      -0.25, -0.25,  8.00,
-      -0.25,  0.25,  8.00,
-
-       0.25, -0.25,  0.25,
-      -0.25, -0.25,  0.25,
-      -0.25,  0.25,  0.25,
-      -0.25,  0.25,  0.25,
-       0.25,  0.25,  0.25,
-       0.25, -0.25,  0.25,
-
-      -0.25,  0.25,  8.00,
-      -0.25, -0.25,  8.00,
-       0.25, -0.25,  8.00,
-       0.25, -0.25,  8.00,
-       0.25,  0.25,  8.00,
-      -0.25,  0.25,  8.00,
-
-      # Origin
-       0.25, -0.25,  0.25,
-      -0.25, -0.25,  0.25,
-      -0.25, -0.25, -0.25,
-      -0.25, -0.25, -0.25,
-       0.25, -0.25, -0.25,
-       0.25, -0.25,  0.25,
-
-       0.25, -0.25, -0.25,
-      -0.25, -0.25, -0.25,
-      -0.25,  0.25, -0.25,
-      -0.25,  0.25, -0.25,
-       0.25,  0.25, -0.25,
-       0.25, -0.25, -0.25,
-
-      -0.25, -0.25,  0.25,
-      -0.25,  0.25,  0.25,
-      -0.25, -0.25, -0.25,
-      -0.25, -0.25, -0.25,
-      -0.25,  0.25,  0.25,
-      -0.25,  0.25, -0.25
-    ]
-
-    self.buffer = np.array(data_list, dtype=np.float32)
+    # TODO: Notice that this has to be after the Entity.__init__ call because I am replacing self.buffer
+    p = STLParser()
+    mesh = Mesh.from_file(p, './robot/visual/meshes/frame.stl')
+    self.buffer = Buffer.from_meshes(mesh)
 
   def load(self):
-    self.build_buffer()
-
-    glBindVertexArray(self.vao)
-
-    glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
-    glBufferData(GL_ARRAY_BUFFER, self.buffer.nbytes, self.buffer, GL_STATIC_DRAW)
-
-    glVertexAttribPointer(self.shader_program.attribute_location('vin_position'), 3, GL_FLOAT, GL_FALSE, 12, None)
-    glEnableVertexAttribArray(self.shader_program.attribute_location('vin_position'))
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0)
-    glBindVertexArray(0)
+    self.buffer.set_attribute_locations(self.shader_program)
+    self.buffer.load()
 
   def update(self, delta):
     pass
 
   def draw(self):
-    with self.shader_program as sp:
+    with self.shader_program as sp, self.buffer:
       sp.uniforms.model_matrix = self.frame
       sp.uniforms.scale_matrix = Matrix4([
         self.scale, 0, 0, 0,
@@ -200,8 +41,6 @@ class FrameEntity(Entity):
         0, 0, 0, 1
       ])
       sp.uniforms.in_opacity = 1.
-
-      glBindVertexArray(self.vao)
 
       # TODO: This is a very hacky way to handle coloring
       sp.uniforms.color_in     = Vector3(0.5, 0, 0)
@@ -213,5 +52,3 @@ class FrameEntity(Entity):
 
       sp.uniforms.color_in     = Vector3(1, 1, 0)
       glDrawArrays(GL_TRIANGLES, 108, 18)
-
-      glBindVertexArray(0)
