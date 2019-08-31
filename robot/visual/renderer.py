6efbbb38bbf34c390106b8cee6aaa5c6ac131488
James Schwartz <jbschwartz@users.noreply.github.com>
2019-08-31 09:49:44 -0400

    Move rendering related tasks from Scene to Renderer.

--- robot/visual/renderer.py
+++ robot/visual/renderer.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 from typing      import Callable, Iterable
 
-from OpenGL.GL import glDrawArrays, GL_TRIANGLES
+from OpenGL.GL import *
 
 from robot.common                    import logger, Timer
 from robot.visual.messaging.listener import listen, listener
@@ -43,9 +43,25 @@ class Renderer():
         raise
 
   @listen(Event.START_FRAME)
+  def start_frame(self):
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+  @listen(Event.START_FRAME)
   def load_buffer_objects(self):
     for ubo in self.ubos:
       ubo.load()
+
+  @listen(Event.START_RENDERER)
+  def start_renderer(self):
+    glClearColor(1, 1, 1, 1)
+
+    capabilities = [GL_DEPTH_TEST, GL_MULTISAMPLE, GL_BLEND, GL_CULL_FACE]
+    for capability in capabilities:
+      glEnable(capability)
+
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+    glCullFace(GL_BACK)
+    glFrontFace(GL_CCW)
 
   @listen(Event.START_RENDERER)
   def bind_buffer_objects(self) -> None:
@@ -102,3 +118,8 @@ class Renderer():
           entity.per_instance(instance, sp, **kwargs)
 
           glDrawArrays(entity.draw_mode, 0, len(entity.buffer))
+
+  @listen(Event.WINDOW_RESIZE)
+  def window_resize(self, width, height):
+    if width and height:
+      glViewport(0, 0, width, height)
--- robot/visual/scene.py
+++ robot/visual/scene.py
@@ -29,28 +29,6 @@ class Scene():
     else:
       return None
 
-  @listen(Event.WINDOW_RESIZE)
-  def window_resize(self, width, height):
-    if width and height:
-      glViewport(0, 0, width, height)
-
-  @listen(Event.START_RENDERER)
-  def start_renderer(self):
-    glClearColor(1, 1, 1, 1)
-
-    glEnable(GL_DEPTH_TEST)
-    glEnable(GL_MULTISAMPLE)
-    glEnable(GL_BLEND)
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-
-    glEnable(GL_CULL_FACE)
-    glCullFace(GL_BACK)
-    glFrontFace(GL_CCW)
-
-  @listen(Event.START_FRAME)
-  def start_frame(self):
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-
   @listen(Event.DRAW)
   def draw(self):
     self.light.position = self.camera.position
