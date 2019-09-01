400418d1655845b613fe69bc1cc7d9c5f6835c69
James Schwartz <jbschwartz@users.noreply.github.com>
2019-09-01 17:27:22 -0400

    Give Rectangle a name.
    Useful for debugging.

--- robot/visual/gui/rectangle.py
+++ robot/visual/gui/rectangle.py
@@ -3,8 +3,9 @@ from typing import Iterable
 from robot.spatial import Vector3
 
 class Rectangle():
-  def __init__(self, position: Vector3, width: float, height: float, color: Iterable[float]) -> None:
-    self.position = position
+  def __init__(self, name: str, position: Vector3, width: float, height: float, color: Iterable[float]) -> None:
+    self.name     = name
+    self.position = position # Top left corner of the rectangle
     self.width    = width
     self.height   = height
     self.color    = color
