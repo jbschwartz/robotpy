721c1d5fe95ae720703843ba34ac84e904370112
James Schwartz <jbschwartz@users.noreply.github.com>
2019-09-08 14:41:54 -0400

    Don't bother with different Slider types for the time being.

--- robot/visual/gui/widgets/slider.py
+++ robot/visual/gui/widgets/slider.py
@@ -13,24 +13,18 @@ from .rectangle import Rectangle
 
 SliderInterval = namedtuple('SliderInterval', 'min max')
 
-class SliderType(enum.Enum):
-  VERTICAL   = 0
-  HORIZONTAL = 1
-
 @listener
 class Slider(Widget):
   def __init__(
     self,
     callback: Callable = None,
     value: float = None,
-    slider_type: SliderType = None,
     **options: dict
   ) -> None:
     super().__init__(**options)
 
-    self._value      = value       or 0.5
-    self.slider_type = slider_type or SliderType.HORIZONTAL
-    self.callback    = callback    or None
+    self._value      = value    or 0.5
+    self.callback    = callback or None
 
     self.add(Rectangle('Range', position=Vector3(0, 0.45), width=1, height=0.1, color=[0.65] * 3))
     self.add(Rectangle('Button', position=Vector3(0.5-0.125, 0.5-0.125), width=0.25, height=0.25, color=[0.25] * 3))
@@ -62,7 +56,7 @@ class Slider(Widget):
   @listen(Event.DRAG)
   def on_drag(self, button, cursor, cursor_delta, modifiers):
     if button == glfw.MOUSE_BUTTON_LEFT and self.children['Button'].hover:
-      cursor_delta[self.slider_type.value] = 0
+      cursor_delta.y = 0
 
       delta = self.screen_cursor(cursor_delta)
       self.children['Button']._position.x -= delta.x / self.width
