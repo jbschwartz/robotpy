ca1fd6a54ea68cabc5ebb434d799c4ab8c62f882
James Schwartz <jbschwartz@users.noreply.github.com>
2019-09-11 20:04:06 -0400

    Highlight the currently selected robot.

--- robot/mech/views/serial_view.py
+++ robot/mech/views/serial_view.py
@@ -3,14 +3,25 @@ from robot.visual.opengl.shader_program import ShaderProgram
 
 class SerialView():
   def __init__(self, serial):
-    self.serial = serial
-    self.visible = True
+    self.serial       = serial
+    self.visible      = True
     self.visibilities = []
-    self.color = [1, 0.5, 0]
-    self.colors = []
-    self.selected = None
-    self.children = []
+    self.color        = [1, 0.5, 0]
+    self.colors       = []
+    self.selected     = None
+    self.children     = []
+    self.highlighted  = False
+
+  def select(self) -> None:
+    self.highlighted = True
+
+  def deselect(self) -> None:
+    self.highlighted = False
 
   def prepare(self, sp: ShaderProgram):
     sp.uniforms.model_matrices  = self.serial.poses()
-    sp.uniforms.link_colors     = [self.color] * 7
+
+    if self.highlighted:
+      sp.uniforms.link_colors = [[0, 1, 1]] * 7
+    else:
+      sp.uniforms.link_colors     = [self.color] * 7
