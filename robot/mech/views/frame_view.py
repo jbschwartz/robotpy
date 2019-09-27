526a6d610d3375eefacb289f8581b9c6f77f2ebd
James Schwartz <jbschwartz@users.noreply.github.com>
2019-09-26 22:10:54 -0400

    Implement Serial frames and keys to show and hide them.

--- robot/mech/views/frame_view.py
+++ robot/mech/views/frame_view.py
@@ -1,16 +1,18 @@
+from typing import Callable, Optional
+
 from robot.spatial import Matrix4
 from robot.visual.opengl.shader_program import ShaderProgram
 
 class FrameView():
-  def __init__(self, serial) -> None:
-    self.serial = serial
-    self.scale = 15
+  def __init__(self, get_transform: Callable, scale: Optional[float] = None) -> None:
+    self.get_transform = get_transform
+    self.scale = scale or 15
 
     self.children = []
     self.visible = True
 
   def prepare(self, sp: ShaderProgram):
-    sp.uniforms.model_matrix = self.serial.pose()
+    sp.uniforms.model_matrix = self.get_transform()
 
     # TODO: Make this happen at the buffer level so this does not need to be called per frame
     # Unless we actually want per frame scaling (often times we don't)
--- robot/mech/views/serial_view.py
+++ robot/mech/views/serial_view.py
@@ -1,6 +1,11 @@
+import glfw
+
+from robot.visual.messaging.listener    import listen, listener
+from robot.visual.messaging.event       import Event
 from robot.visual.opengl.shader_program import ShaderProgram
 from .frame_view                        import FrameView
 
+@listener
 class SerialView():
   def __init__(self, serial):
     # TODO: Decide if this should also be a Widget
@@ -12,8 +17,12 @@ class SerialView():
     self.color        = [1, 0.5, 0]
     self.colors       = []
     self.selected     = None
+
+    # Pass a lambda so that the transformation updates as the robot moves
+    # Passing the transformation directly does not work since the robot reassigns
+    # its link transformations every frame.
     self.children     = [
-      FrameView(self.serial)
+      *[FrameView(lambda l=link: l.to_world) for link in self.serial.links]
     ]
     self.highlighted  = False
 
@@ -30,3 +39,11 @@ class SerialView():
       sp.uniforms.link_colors = [[0, 1, 1]] * 7
     else:
       sp.uniforms.link_colors     = [self.color] * 7
+
+  @listen(Event.KEY)
+  def key(self, key, action, modifiers):
+    if action == glfw.PRESS and self.highlighted:
+      for key_number in range(1, 8):
+        if key == getattr(glfw, f'KEY_{key_number}'):
+          self.children[key_number - 1].visible = not self.children[key_number - 1].visible
+          return
